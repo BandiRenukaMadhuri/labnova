fa7ea31c46542041f997cc135fc12251d5f933fa
Bandi Renuka Madhhuri <renwkamadhuri1226@gmail.com>
2024-11-27 13:12:17 +0530

    responsive Tea website

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,32 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
-            <div className="about-Us">
-                <h1>Empowering Diagnostic Innovation</h1>
-                <div className="about-Us-content">
-                    <h2>With years of experience in the industry, Labnova brings expertise in developing cutting-edge diagnostic devices and reagents, designed to meet the evolving needs of the IVD industry.</h2>
-                    <img src=""/>
-                    <p>Driven by the vision of ‘make in India’ and the directive of the Prime Minister, Labnova is committed to developing and manufacturing innovative POCT & Critical Care devices, adhering to the highest standards of quality and excellence.
-
-Labnova has proudly served a diverse range of clients in the healthcare sector, including hospitals, clinics, and diagnostic laboratories.</p>
-                    </div>
-
-            </div>
+        <div className="about-Us">
+    <h1>Empowering Diagnostic Innovation</h1>
+    <div className="about-Us-content">
+    <div className="image-content">
+            <img src="src/assets/pexels-photo-about.jpeg" alt="About Us" />
         </div>
+        <div className="text-content">
+            <h2>
+                With years of experience in the industry, Labnova brings expertise in
+                developing cutting-edge diagnostic devices and reagents, designed to meet
+                the evolving needs of the IVD industry.
+            </h2>
+            <p>
+                Driven by the vision of ‘make in India’ and the directive of the Prime
+                Minister, Labnova is committed to developing and manufacturing innovative
+                POCT & Critical Care devices, adhering to the highest standards of quality
+                and excellence.
+                <br />
+                <br />
+                Labnova has proudly served a diverse range of clients in the healthcare
+                sector, including hospitals, clinics, and diagnostic laboratories.
+            </p>
+        </div>
+    </div>
+</div>
+
     )
     
 }
